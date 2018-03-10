509b601199fbd0e21f71107c7b320ac4ee3fe5d8
pobx <geidtiphong@gmail.com>
2018-03-10 13:30:44 +0700

    create loop menu purchase-items

--- src/pages/purchase-items/purchase-items.ts
+++ src/pages/purchase-items/purchase-items.ts
@@ -10,7 +10,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PurchaseItemsPage {
 
+  cards: Object[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.cards = [
+      {img: './assets/imgs/logo.png', name:'ค้นหาผู้ขายขยะ'},
+      {img: './assets/imgs/logo.png', name:'ประวัติรับซื้อขยะ'},
+    ]
   }
 
   ionViewDidLoad() {
