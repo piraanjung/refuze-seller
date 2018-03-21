5d778c1fd3f8a8a250ae545db69f66969985d824
pobx <geidtiphong@gmail.com>
2018-03-21 20:27:05 +0700

    remove Observeable

--- src/providers/authen/authen.ts
+++ src/providers/authen/authen.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs/Observable";
 import { API_URL } from '../api-urls';
 import { Buyer } from '../../models/buyer';
 @Injectable()
@@ -8,7 +7,7 @@ export class AuthenProvider {
 
   constructor(private http: HttpClient) { }
 
-  resAuthen(params) {
+  resAuthen(params): any {
     return this.http.post<Buyer>(`${API_URL}/authen`, params);
   }
 
