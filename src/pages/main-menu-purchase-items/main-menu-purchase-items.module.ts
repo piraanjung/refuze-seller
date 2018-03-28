f670ad4657339a03bfb2973510f26ac0ae32711b
pobx <geidtiphong@gmail.com>
2018-03-28 19:13:43 +0700

    remove main-menu-purchase-item.module

--- src/pages/main-menu-purchase-items/main-menu-purchase-items.module.ts
+++ src/pages/main-menu-purchase-items/main-menu-purchase-items.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MainMenuPurchaseItemsPage } from './main-menu-purchase-items';
-import { MainMenuPurchaseItemProvider } from "../../providers/main-menu-purchase-item/main-menu-purchase-item";
 @NgModule({
   declarations: [
     MainMenuPurchaseItemsPage,
   ],
   imports: [
     IonicPageModule.forChild(MainMenuPurchaseItemsPage),
-
   ],
-  providers:[
-    MainMenuPurchaseItemProvider
-  ]
+  
 })
 export class MainMenuPurchaseItemsPageModule {}
