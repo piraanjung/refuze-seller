5a289affbff82f6c3fa9d555cfbf6c5bbf3d7999
piraanjung <piraanj@gmail.com>
2018-03-28 22:15:19 +0700

    add btn class

--- src/pages/main-menu-purchase-items/main-menu-purchase-items.module.ts
+++ src/pages/main-menu-purchase-items/main-menu-purchase-items.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MainMenuPurchaseItemsPage } from './main-menu-purchase-items';
-import { MainMenuPurchaseItemProvider } from "../../providers/main-menu-purchase-item/main-menu-purchase-item";
 @NgModule({
   declarations: [
     MainMenuPurchaseItemsPage,
@@ -9,9 +8,6 @@ import { MainMenuPurchaseItemProvider } from "../../providers/main-menu-purchase
   imports: [
     IonicPageModule.forChild(MainMenuPurchaseItemsPage),
 
-  ],
-  providers:[
-    MainMenuPurchaseItemProvider
   ]
 })
 export class MainMenuPurchaseItemsPageModule {}
