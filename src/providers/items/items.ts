e5d3359c9bc844895b0c5a8e4042e0d569f3bd58
pobx <geidtiphon@gmail.com>
2018-03-31 12:27:22 +0700

    add header to all providers in application

--- src/providers/items/items.ts
+++ src/providers/items/items.ts
@@ -3,21 +3,28 @@ import { Injectable } from '@angular/core';
 import { API_URL } from '../api-urls';
 import { Observable } from 'rxjs/Observable';
 import { Item, ItemPrice } from '../../models/item';
+import { Buyer } from '../../models/buyer';
 
 @Injectable()
 export class ItemsProvider {
 
-  constructor(private http: HttpClient) { }
+  API_HEADERS: any
+  constructor(private http: HttpClient) {
+    let buyerProfile: Buyer = JSON.parse(localStorage.getItem('buyerProfile'))
+    this.API_HEADERS = {
+      Authorization: buyerProfile.remember_token
+    }
+  }
 
   getFavorite() {
-    return this.http.get<Item[]>(`${API_URL}/items-favorite`)
+    return this.http.get<Item[]>(`${API_URL}/items-favorite`, { headers: this.API_HEADERS })
   }
 
   getItems() {
-    return this.http.get<Item[]>(`${API_URL}/items`)
+    return this.http.get<Item[]>(`${API_URL}/items`, { headers: this.API_HEADERS })
   }
 
   setItemPrice(params: ItemPrice) {
-    return this.http.put(`${API_URL}/items-update-price`, params)
+    return this.http.put(`${API_URL}/items-update-price`, params, { headers: this.API_HEADERS })
   }
 }
--- src/providers/purchase-items/purchase-items.ts
+++ src/providers/purchase-items/purchase-items.ts
@@ -3,18 +3,25 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { API_URL } from '../api-urls';
 import { PurchaseTransactionHistory } from '../../models/purchase-items';
+import { Buyer } from '../../models/buyer';
 
 @Injectable()
 export class PurchaseItemsProvider {
 
-  constructor(private http: HttpClient) { }
+  API_HEADERS: any
+  constructor(private http: HttpClient) {
+    let buyerProfile: Buyer = JSON.parse(localStorage.getItem('buyerProfile'))
+    this.API_HEADERS = {
+      Authorization: buyerProfile.remember_token
+    }
+  }
 
   createPurchaseProfile(params) {
-    return this.http.post(`${API_URL}/purchase-items/create`, params)
+    return this.http.post(`${API_URL}/purchase-items/create`, params, { headers: this.API_HEADERS })
   }
 
   getPurchaseTransactionsHistoryBySellerId(id) {
-    return this.http.get<PurchaseTransactionHistory[]>(`${API_URL}/purchases-items/history/seller/${id}`)
+    return this.http.get<PurchaseTransactionHistory[]>(`${API_URL}/purchases-items/history/seller/${id}`, { headers: this.API_HEADERS })
   }
 
 }
