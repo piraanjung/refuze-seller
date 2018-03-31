c9f6eec3baf5888c8e92766f6260a2ef293fe3d2
piraanjung <piraanj@gmail.com>
2018-03-31 12:08:27 +0700

    main-menu-purchase-items layouts

--- src/providers/_main-menu-purchase-items.ts
+++ src/providers/_main-menu-purchase-items.ts
@@ -3,11 +3,13 @@ export const MAIN_MENU_PURCHASE_ITEMS = {
     {
       "image": "assets/images/background/0.jpg",
       "title": "ค้นหาผู้ขายขยะ",
+      "title_button" :"ค้นหาผู้ขายขยะ",
       "link": 'find-seller'
     },
     {
       "image": "assets/images/background/9.jpg",
       "title": "ประวัติรับซื้อขยะ",
+      "title_button" :"ดูประวัติรับซื้อขยะ",
       "link": '#'
     },
   ]
