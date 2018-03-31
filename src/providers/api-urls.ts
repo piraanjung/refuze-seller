20729651a7ae25c17d43796e1a5fc103368f459b
pobx <geidtiphon@gmail.com>
2018-03-31 12:17:28 +0700

    get bueryProfile in constructor on class find-sellers

--- src/providers/api-urls.ts
+++ src/providers/api-urls.ts
@@ -1,20 +1,2 @@
-
-import { Buyer } from '../models/buyer'
-let buyerProfile: Buyer
-buyerProfile = {
-  id: 0,
-  remember_token: ''
-}
-
-if (localStorage.getItem('buyerProfile') != null) {
-  let buyerLocalStorage: Buyer = JSON.parse(localStorage.getItem('buyerProfile'));
-  buyerProfile.remember_token = buyerLocalStorage.remember_token
-}
-
-export const API_HEADERS = {
-  Authorization: buyerProfile.remember_token
-  // 'Authorization': 'SXZHRlpESlR1dGZEU09kVFJoWkVpMU1HMzV1UlZJakU1NkZoeTlFNw=='
-}
-
 // export const API_URL = 'http://188.166.177.172/refuze-api/public/index.php';
 export const API_URL = 'http://localhost:9000';
