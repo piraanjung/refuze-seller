1e68130c0fbe8e56fdf9bfa78253e790a9b56163
piraanjung <piraanj@gmail.com>
2018-04-12 16:56:53 +0700

    add module create qrcode

--- src/pages/sell-trash/sell-trash.module.ts
+++ src/pages/sell-trash/sell-trash.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SellTrashPage } from './sell-trash';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 @NgModule({
   declarations: [
@@ -8,6 +10,10 @@ import { SellTrashPage } from './sell-trash';
   ],
   imports: [
     IonicPageModule.forChild(SellTrashPage),
+    NgxQRCodeModule
   ],
+  providers:[
+    BarcodeScanner
+  ]
 })
 export class SellTrashPageModule {}
--- src/pages/sell-trash/sell-trash.ts
+++ src/pages/sell-trash/sell-trash.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the SellTrashPage page.
@@ -16,12 +17,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'sell-trash.html',
 })
 export class SellTrashPage {
+  show_qr_code: boolean = false;
+  qrData = null;
+  createCode = null;
+  
+  seller_code:any =''
+  user:any =[]
+  data=[
+    {
 
+    }
+  ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SellTrashPage');
+  }
+
+  onEvent(event: string, result:any) {
+    if (event) {
+        // this.events[event](result);
+    }
+  }
+
+  generate_qrcode(){
+    // this.storage.get('currentUser').then(res =>{
+    //   this.user =res
+    //   console.log(this.user.mobile)
+    //   if(this.user.mobile != ""){
+        this.createCode = '122222';//this.user.mobile
+        this.show_qr_code = true
+    //   }
+      
+    // })
   }
 
 }
