2651bc5cc0b030d428e334d0969825f030b0b6cd
pobx <geidtiphong@gmail.com>
2018-06-08 17:18:57 +0700

    edit layout account-transfer html

--- src/pages/account-transfer/account-transfer.ts
+++ src/pages/account-transfer/account-transfer.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Sellers } from '../../models/sellers';
 
 @IonicPage({
   name: 'account-transfer'
@@ -9,15 +10,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'account-transfer.html',
 })
 export class AccountTransferPage {
+  seller: Sellers
+  seller_name: string
+  mobile: string
+  image: string
+  cash: number
   BackPage: string
   NextPage: string
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.BackPage = 'account-perform'
     this.NextPage = 'account-transfer-confirm'
+    this.cash = 900
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AccountTransferPage');
+    this.seller = JSON.parse(localStorage.getItem('sellerProfile')) || {};
+    this.seller_name = `${this.seller.name} ${this.seller.last_name}`
+    this.mobile = this.seller.mobile
+    this.image = this.seller.image_url
   }
 
 }
