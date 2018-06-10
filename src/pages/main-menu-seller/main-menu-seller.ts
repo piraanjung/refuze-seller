42f9b6a37bbf264ffae9eacb026e5a9d53b4c298
pobx <geidtiphong@gmail.com>
2018-06-10 10:53:46 +0700

    refactor code on main-menu-seller

--- src/pages/main-menu-seller/main-menu-seller.ts
+++ src/pages/main-menu-seller/main-menu-seller.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 
 @IonicPage({
   name: 'main-menu-seller'
@@ -12,56 +12,38 @@ export class MainMenuSellerPage {
 
   data: any = [
     {
-      "title_description": "",
-      "description": "",
-      "buttonText": "",
       "headerImage": "assets/images/background/sell-garbage.png",
       "title": "ขายขยะ",
       "navCtrl": "sell-trash",
     },
     {
-      "title_description": "",
-      "description": "",
-      "buttonText": "",
       "headerImage": "assets/images/background/garbagebank.png",
       "title": "ธนาคารขยะ",
       "navCtrl": "account-balance",
     },
     {
-      "title_description": "",
-      "description": "",
-      "buttonText": "PROCEED",
       "headerImage": "assets/images/background/shoppingcart.png",
       "title": "ซื้อสินค้า ออนไลน์",
       "navCtrl": "shopping-cart-main",
     },
     {
-      "title_description": "",
-      "description": "",
-      "buttonText": "",
       "headerImage": "assets/images/background/localproduct.png",
       "title": "ซื้อสินค้าในโรงเรียน",
       "navCtrl": "buy-local-product",
     },
     {
-      "title_description": "",
-      "description": "",
-      "buttonText": "",
       "headerImage": "assets/images/background/estimated_cost.png",
       "title": "ราคากลางขยะ",
       "navCtrl": "find-items",
     },
     {
-      "title_description": "",
-      "description": "",
-      "buttonText": "",
       "headerImage": "assets/images/background/sellhistory.png",
       "title": "ประวัติการขายขยะ",
       "navCtrl": "sell-history",
     }
   ]
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController) {
   }
 
   ionViewDidLoad() {
