4c452bb4a2e5c9df4fed7941ab98786a94adc938
pobx <geidtiphong@gmail.com>
2018-06-10 11:04:38 +0700

    formatting account balance

--- src/pages/account-balance/account-balance.ts
+++ src/pages/account-balance/account-balance.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 import { Sellers } from '../../models/sellers';
 import { AccountSavingProvider } from '../../providers/account-saving/account-saving';
 import { LoadingPageProvider } from '../../providers/loading-page';
@@ -28,14 +28,14 @@ export class AccountBalancePage {
   }
 
   ionViewDidLoad() {
-    this.seller = JSON.parse(localStorage.getItem('sellerProfile')) || {};
+    this.seller = JSON.parse(localStorage.getItem('sellerProfile')) || {}
     this.seller_name = `${this.seller.name} ${this.seller.last_name}`
     this.mobile = this.seller.mobile
     this.image = this.seller.image_url
-    
+
     localStorage.removeItem('AccountReceiveTransfer')
     localStorage.removeItem('CashInput')
-    
+
     this.getAccountSaving()
   }
 
@@ -43,13 +43,14 @@ export class AccountBalancePage {
     let loading = this.loading.loading()
     let user_id = this.seller.id || 0
     loading.present()
-    this.accountSaving.getAccountSavingBalance(user_id).subscribe(res => {
-      this.balance = res.balance
-      loading.dismiss()
-    }, err => {
-      console.log(err)
-      loading.dismiss()
-    })
+    this.accountSaving.getAccountSavingBalance(user_id)
+      .subscribe(res => {
+        this.balance = res.balance
+        loading.dismiss()
+      }, err => {
+        console.log(err)
+        loading.dismiss()
+      })
   }
 
 }
