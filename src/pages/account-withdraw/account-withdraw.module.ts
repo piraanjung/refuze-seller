d1b5b3322d5039da709e6970fec8b527e046fcb4
pobx <geidtiphong@gmail.com>
2018-06-14 17:56:48 +0700

    create account-withdraw OK

--- src/pages/account-withdraw/account-withdraw.module.ts
+++ src/pages/account-withdraw/account-withdraw.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
 import { AccountWithdrawPage } from './account-withdraw';
+import { AccountSavingProvider } from '../../providers/account-saving/account-saving';
+import { LoadingPageProvider } from '../../providers/loading-page';
+import { AlertBoxProvider } from '../../providers/alert-box';
 
 @NgModule({
   declarations: [
@@ -8,6 +12,8 @@ import { AccountWithdrawPage } from './account-withdraw';
   ],
   imports: [
     IonicPageModule.forChild(AccountWithdrawPage),
+    HttpClientModule,
   ],
+  providers: [AccountSavingProvider, LoadingPageProvider, AlertBoxProvider]
 })
-export class AccountWithdrawPageModule {}
+export class AccountWithdrawPageModule { }
--- src/pages/account-withdraw/account-withdraw.ts
+++ src/pages/account-withdraw/account-withdraw.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertBoxProvider } from '../../providers/alert-box';
+import { LoadingPageProvider } from '../../providers/loading-page';
+import { AccountSavingProvider } from '../../providers/account-saving/account-saving';
+import { Sellers } from '../../models/sellers';
 
 @IonicPage({
   name: 'account-withdraw'
@@ -10,11 +14,66 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AccountWithdrawPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  seller: Sellers
+  seller_name: string
+  mobile: string
+  image: string
+  cash_input: number
+  balance: number
+  label_balance: number
+  balance_less_than: number
+  params: Object
+  BackPage: string
+  account_saving_id: number
+
+  constructor(
+    private navCtrl: NavController,
+    private alertBox: AlertBoxProvider,
+    private loading: LoadingPageProvider,
+    private accountSaving: AccountSavingProvider
+  ) {
+    this.BackPage = 'account-perform'
+    this.balance = 0
+    this.label_balance = 0
+    this.params = { cash_input: this.cash_input }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AccountWithdrawPage');
+    this.seller = JSON.parse(localStorage.getItem('sellerProfile')) || {}
+    this.seller_name = `${this.seller.name} ${this.seller.last_name}`
+    this.mobile = this.seller.mobile
+    this.image = this.seller.image_url
+    this.cash_input = 0
+    this.balance_less_than = 100
+    this.account_saving_id = this.seller.account_saving_id || 0
+    
+    this.getAccountSaving()
+  }
+
+  getAccountSaving() {
+    let loading = this.loading.loading()
+    loading.present()
+    this.accountSaving.getAccountSavingBalanceByAccountId(this.account_saving_id)
+      .subscribe(res => {
+        if (res.status == 200) {
+          this.label_balance = res.body.balance
+          this.balance = res.body.balance
+        } else if (res.status == 204) {
+          this.alertBox.showAlert('ไม่พบข้อมูลรายการของบัญชีของคุณ')
+        } else {
+          this.alertBox.showAlert('ไม่สามารถดำเนินรายการได้ กรุณาลองใหม่ภายหลังต่ะ')
+        }
+
+        loading.dismiss()
+      }, err => {
+        this.alertBox.showAlert('ไม่สามารถดำเนินรายการได้ กรุณาลองใหม่ภายหลังต่ะ')
+        console.log(err)
+        loading.dismiss()
+      })
+  }
+
+  private withDrawMoney() {
+    
   }
 
 }
