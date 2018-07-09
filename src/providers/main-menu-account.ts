811b30fdf2c69852c4fb50bd880a8a2dfa9a755f
Pobx <geidtiphong@gmail.com>
2018-07-09 17:56:38 +0700

    create main-menu-account.ts for storage pages

---
+++ src/providers/main-menu-account.ts
@@ -0,0 +1,43 @@
+export const MAIN_MENU_SELLER = [
+  {
+    "headerImage": "assets/images/background/sell-garbage.png",
+    "title": "ขายขยะ",
+    "navCtrl": "sell-trash",
+    "icon": "cart"
+  },
+  {
+    "headerImage": "assets/images/background/garbagebank.png",
+    "title": "ธนาคารขยะ",
+    "navCtrl": "account-balance",
+    "icon": "logo-bitcoin"
+  },
+  {
+    "headerImage": "assets/images/background/shoppingcart.png",
+    "title": "ซื้อสินค้าออนไลน์",
+    "navCtrl": "shopping-cart-main",
+    "icon": "cloud"
+  },
+  {
+    "headerImage": "assets/images/background/localproduct.png",
+    "title": "ซื้อสินค้าในโรงเรียน",
+    "navCtrl": "buy-local-product",
+    "icon": "home"
+  },
+  {
+    "headerImage": "assets/images/background/estimated_cost.png",
+    "title": "ราคากลางขยะ",
+    "navCtrl": "find-items",
+    "icon": "stats"
+  },
+  {
+    "headerImage": "assets/images/background/sellhistory.png",
+    "title": "ประวัติการขายขยะ",
+    "navCtrl": "sell-history",
+    "icon": "recording"
+  },
+  {
+    "title": "ออกจากระบบ",
+    "navCtrl": "AuthenticationPage",
+    "icon": "exit"
+  }
+]
