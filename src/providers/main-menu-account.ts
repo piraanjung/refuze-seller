5ff5f7617704de03050449d2171b28c961849216
Pobx <geidtiphong@gmail.com>
2018-07-09 18:05:34 +0700

    edit main-menu-account OK

--- src/providers/main-menu-account.ts
+++ src/providers/main-menu-account.ts
@@ -1,43 +1,27 @@
-export const MAIN_MENU_SELLER = [
+export const MAIN_MENU_ACCOUNT = [
   {
-    "headerImage": "assets/images/background/sell-garbage.png",
-    "title": "ขายขยะ",
-    "navCtrl": "sell-trash",
-    "icon": "cart"
-  },
-  {
-    "headerImage": "assets/images/background/garbagebank.png",
     "title": "ธนาคารขยะ",
     "navCtrl": "account-balance",
     "icon": "logo-bitcoin"
   },
   {
-    "headerImage": "assets/images/background/shoppingcart.png",
-    "title": "ซื้อสินค้าออนไลน์",
-    "navCtrl": "shopping-cart-main",
-    "icon": "cloud"
+    "title": "โอนเงิน",
+    "navCtrl": "account-transfer",
+    "icon": "logo-bitcoin"
   },
   {
-    "headerImage": "assets/images/background/localproduct.png",
-    "title": "ซื้อสินค้าในโรงเรียน",
-    "navCtrl": "buy-local-product",
-    "icon": "home"
+    "title": "ถอนเงิน",
+    "navCtrl": "account-withdraw",
+    "icon": "logo-bitcoin"
   },
   {
-    "headerImage": "assets/images/background/estimated_cost.png",
-    "title": "ราคากลางขยะ",
-    "navCtrl": "find-items",
-    "icon": "stats"
+    "title": "สรุปธุรกรรมการเงิน",
+    "navCtrl": "account-statements",
+    "icon": "logo-bitcoin"
   },
   {
-    "headerImage": "assets/images/background/sellhistory.png",
-    "title": "ประวัติการขายขยะ",
-    "navCtrl": "sell-history",
-    "icon": "recording"
-  },
-  {
-    "title": "ออกจากระบบ",
-    "navCtrl": "AuthenticationPage",
-    "icon": "exit"
+    "title": "ออกจากธนาคารขยะ",
+    "navCtrl": "main-menu-seller",
+    "icon": "hologo-bitcoinme"
   }
 ]
