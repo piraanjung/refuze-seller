d127847dbd5708a6df53e1672e6462c15a8e7187
Pobx <geidtiphong@gmail.com>
2018-07-09 18:23:16 +0700

    change navPush api to setRoot on account-balance

--- src/pages/account-balance/account-balance.ts
+++ src/pages/account-balance/account-balance.ts
@@ -15,64 +15,64 @@ import { MAIN_MENU_ACCOUNT } from '../../providers/main-menu-account';
 })
 export class AccountBalancePage {
 
-  AccountPerform: string
-  seller: Sellers
-  seller_name: string
-  mobile: string
-  image: string
-  balance: number
-  account_saving_id: number
-  headerSideBar: string
+  seller: Sellers;
+  seller_name: string;
+  mobile: string;
+  image: string;
+  balance: number;
+  account_saving_id: number;
+  headerSideBar: string;
 
   constructor(
+    private events: Events,
+    private menuCtrl: MenuController,
+    private navCtrl: NavController,
     private accountSaving: AccountSavingProvider,
     private alertBox: AlertBoxProvider,
     private loading: LoadingPageProvider,
-    private events: Events,
-    private menuCtrl: MenuController
   ) {
     this.headerSideBar = 'ธนาคารขยะ';
     this.events.publish('header-side-bar', this.headerSideBar);
     this.events.publish('pages', MAIN_MENU_ACCOUNT);
     this.menuCtrl.enable(true, 'menu-side-bar');
-    this.AccountPerform = 'account-perform'
-    this.balance = 0
-
-    
+    this.balance = 0;
   }
 
   ionViewDidLoad() {
-    this.seller = JSON.parse(localStorage.getItem('sellerProfile')) || {}
-    this.seller_name = `${this.seller.name} ${this.seller.last_name}`
-    this.mobile = this.seller.mobile
-    this.image = this.seller.image_url
-    this.account_saving_id = this.seller.account_saving_id || 0
+    this.seller = JSON.parse(localStorage.getItem('sellerProfile')) || {};
+    this.seller_name = `${this.seller.name} ${this.seller.last_name}`;
+    this.mobile = this.seller.mobile;
+    this.image = this.seller.image_url;
+    this.account_saving_id = this.seller.account_saving_id || 0;
 
-    localStorage.removeItem('AccountReceiveTransfer')
-    localStorage.removeItem('CashInput')
+    localStorage.removeItem('AccountReceiveTransfer');
+    localStorage.removeItem('CashInput');
 
-    this.getAccountSaving()
+    this.getAccountSaving();
   }
 
   getAccountSaving() {
-    let loading = this.loading.loading()
-    loading.present()
+    let loading = this.loading.loading();
+    loading.present();
     this.accountSaving.getAccountSavingBalanceByAccountId(this.account_saving_id)
       .subscribe(res => {
         if (res.status == 200) {
-          this.balance = res.body.balance
-        }else if (res.status == 204) {
-          this.alertBox.showAlert('ไม่พบข้อมูลรายการของบัญชีของคุณ')
-        }else {
-          this.alertBox.showAlert('ไม่สามารถดำเนินรายการได้ กรุณาลองใหม่ภายหลังต่ะ')
+          this.balance = res.body.balance;
+        } else if (res.status == 204) {
+          this.alertBox.showAlert('ไม่พบข้อมูลรายการของบัญชีของคุณ');
+        } else {
+          this.alertBox.showAlert('ไม่สามารถดำเนินรายการได้ กรุณาลองใหม่ภายหลังต่ะ');
         }
 
-        loading.dismiss()
+        loading.dismiss();
       }, err => {
-        this.alertBox.showAlert('ไม่สามารถดำเนินรายการได้ กรุณาลองใหม่ภายหลังต่ะ')
-        console.log(err)
-        loading.dismiss()
+        this.alertBox.showAlert('ไม่สามารถดำเนินรายการได้ กรุณาลองใหม่ภายหลังต่ะ');
+        console.log(err);
+        loading.dismiss();
       })
   }
 
+  openPage() {
+    this.navCtrl.setRoot('account-perform', {}, { animate: true, direction: 'forward' });
+  }
 }
